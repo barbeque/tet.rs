913186d9a9b79cbe262f3e34cb082fac3a4271bc
mike stedman <barbeque@users.noreply.github.com>
2019-04-05 22:22:01 -0600

    main loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate sdl2;
+
+use crate::sdl2::event::Event;
+use crate::sdl2::keyboard::Keycode;
+use crate::sdl2::pixels::Color;
+
 fn main() {
-    println!("Hello, world!");
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let audio_subsystem = sdl_context.audio().unwrap();
+
+    let window = video_subsystem
+        .window("tetris", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'main: loop {
+        canvas.clear();
+        canvas.present();
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} | Event::KeyDown {..} => break 'main,
+                _ => {}
+            }
+        }
+    }
 }
